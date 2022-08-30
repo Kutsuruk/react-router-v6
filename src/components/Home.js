133a737e817e02e99ed34fc79c5f7bab028a84b0
Vitalii Kutsuruk <kutsurukvet@gmail.com>
2022-08-30 19:57:15 +0300

    Made navigation programmatically using useNavigate hook. Make button for navigation back throught routes.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
+import {useNavigate} from "react-router-dom";
 
 const Home = () => {
+    const navigate = useNavigate()
+
     return(
         <div>
             Homepage
+
+            <div>
+                <button onClick={() => navigate('order-summary', {replace: true})}>Check order</button>
+            </div>
         </div>
     )
 }
